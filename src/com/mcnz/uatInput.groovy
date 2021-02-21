64642b06e698a640eb0da55a9b153d4e6ceb3c32
nadernashaatn <nadernashaatn@gmail.com>
2021-02-21 18:40:30 +0100

    fix groovy2

--- src/com/mcnz/uatInput.groovy
+++ src/com/mcnz/uatInput.groovy
@@ -6,13 +6,13 @@ package com.mcnz
 
   public class uatInput {
     def buildIsUatApproved() {
-    def file = new File("/approved.txt")
+    def file = new File("approved.txt")
     if (file.exists()){
       return true;
     }
     else {
       println "Approval file does not exist."
-      return true; 
+      return false; 
     }    
   }
 }
