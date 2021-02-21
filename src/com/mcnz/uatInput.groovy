41a4ac7b829c826c8f98b4988cea5a71a4a53ad5
nadernashaatn <nadernashaatn@gmail.com>
2021-02-21 10:38:46 +0100

    add pipeline

---
+++ src/com/mcnz/uatInput.groovy
@@ -0,0 +1,18 @@
+/* 
+* jenkins-shared-library/src/com/mcnz/uatInput.groovy
+* https://github.com/learn-devops-fast/jenkins-shared-library.git
+*/
+package com.mcnz
+
+  public class uatInput {
+    def buildIsUatApproved() {
+    def file = new File("C:/_tools/approved.txt")
+    if (file.exists()){
+      return true;
+    }
+    else {
+      println "Approval file does not exist."
+    } 
+    return false; 
+  }
+}
